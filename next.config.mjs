d84957ad0974faef47ccc8f457e678e8ca5ae8d6
adsz17 <140124825+adsz17@users.noreply.github.com>
2025-08-27 13:04:41 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
