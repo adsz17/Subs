6df840172fbb45f6f39c283853bd4708ef3a8fb8
subscripcioninfo-glitch <subscripcion.info@gmail.com>
2025-08-27 12:14:29 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: { allowedOrigins: ['*'] } }
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(process.cwd()),
+    };
+    return config;
+  },
 };
+
 export default nextConfig;
